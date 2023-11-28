21b47071019d6448316f00061b28c790624c1a9a
Marcos-Floriano <marcos.feu@sptech.school>
2023-11-28 13:41:56 -0300

    incrementando o update no python

--- Processos.py
+++ Processos.py
@@ -43,6 +43,42 @@ def obter_info_processos():
 
     return info_processos
 
+def consultar_processo_por_pid(pid):
+    comando_sql = "SELECT * FROM processos WHERE pid = %s"
+    valores = (pid,)
+
+    try:
+        cursor.execute(comando_sql, valores)
+        resultado = cursor.fetchone()
+
+        if resultado:
+            print(f"Informações do processo com PID {pid}:")
+            print(f"PID: {resultado[0]}")
+            print(f"Data e Hora: {resultado[1]}")
+            print(f"Nome do Processo: {resultado[2]}")
+            print(f"Uso de CPU: {resultado[3]}")
+            print(f"Gravação em Disco: {resultado[4]}")
+            print(f"FK Máquina: {resultado[5]}")
+            print(f"FK Empresa: {resultado[6]}")
+        else:
+            print(f"Processo com PID {pid} não encontrado.")
+
+    except mysql.connector.Error as err:
+        print(f"Falha ao consultar processo por PID: {err}")
+
+def inserir_ou_atualizar_processo(processo):
+    data_hora = datetime.now()
+    comando_sql = "INSERT INTO processos (pid, data_hora, nome_processo, uso_cpu, gravacao_disco, fk_maquinaP, fk_empresaP) VALUES (%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE data_hora = %s, nome_processo = %s, uso_cpu = %s, gravacao_disco = %s, fk_maquinaP = %s, fk_empresaP = %s"
+    valores = (processo['pid'], data_hora, processo['nome'], processo['uso_cpu'], processo['uso_disco'], 1, 1, data_hora, processo['nome'], processo['uso_cpu'], processo['uso_disco'], 1, 1)
+
+    try:
+        cursor.execute(comando_sql, valores)
+        conexao.commit()
+        print(f"Dados inseridos ou atualizados com sucesso para o processo com PID {processo['pid']}")
+    except mysql.connector.Error as err:
+        print(f"Falha ao inserir ou atualizar dados para o processo com PID {processo['pid']}: {err}")
+        conexao.rollback()
+
 # Exemplo de uso
 while True:
     # Calcula o uso total da CPU antes de obter informações do processo
@@ -56,15 +92,11 @@ while True:
         # Calcula o uso da CPU relativo ao intervalo desde a última medição
         uso_cpu_processo = processo['uso_cpu']
 
-        data_hora = datetime.now()
-        comando_sql = "INSERT INTO processos (pid, data_hora, nome_processo, uso_cpu, gravacao_disco, fk_maquinaP, fk_empresaP) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        valores = (processo['pid'], data_hora, processo['nome'], uso_cpu_processo, processo['uso_disco'], 1, 1)
+        # Exemplo de inserção ou atualização por PID
+        inserir_ou_atualizar_processo(processo)
 
-        try:
-            cursor.execute(comando_sql, valores)
-            conexao.commit()
-            print("Dados inseridos com sucesso na tabela processos")
-        except mysql.connector.Error as err:
-            print(f"Falha ao inserir dados na tabela processos: {err}")
-            conexao.rollback()
+    # Exemplo de consulta por PID (substitua 1234 pelo PID desejado)
+    consultar_processo_por_pid(1234)
 
+    # Aguarda 10 segundos antes da próxima iteração
+    time.sleep(10)
