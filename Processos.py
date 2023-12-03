4bba4c0b19e3ed34dd787df8e2761df2f950b244
Marcos-Floriano <marcos.feu@sptech.school>
2023-12-03 09:47:46 -0300

    conectando o python no windows server

--- Processos.py
+++ Processos.py
@@ -1,26 +1,26 @@
+# Importa bibliotecas necessárias
 import psutil
 import threading
-import time
 import keyboard
+import time
 import socket
 import pymssql
 import mysql.connector
 import json
-import requests
-from datetime import datetime
-from cred import usr, pswd  # Certifique-se de ter o módulo cred com as variáveis usr e pswd definidas
+from datetime import datetime as dt
 
 # Configuração da conexão com o MySQL
 conexao = mysql.connector.connect(
     host='localhost',
-    user='aluno',
-    password='sptech',
+    user='root',
+    password='Marnn111',
     database='nocLine'
 )
 
-# Cria um objeto de cursor para executar comandos SQL
+mydb = None
 cursor = conexao.cursor()
 
+# Define a função para obter informações sobre os processos em execução
 def obter_info_processos():
     info_processos = []
 
@@ -50,6 +50,7 @@ def obter_info_processos():
 
     return info_processos
 
+# Define a função para consultar informações de um processo por PID
 def consultar_processo_por_pid(pid):
     comando_sql = "SELECT * FROM processos WHERE pid = %s"
     valores = (pid,)
@@ -73,8 +74,9 @@ def consultar_processo_por_pid(pid):
     except mysql.connector.Error as err:
         print(f"Falha ao consultar processo por PID: {err}")
 
+# Define a função para inserir ou atualizar informações de um processo
 def inserir_ou_atualizar_processo(processo):
-    data_hora = datetime.now()
+    data_hora = dt.now()
     comando_sql = "INSERT INTO processos (pid, data_hora, nome_processo, uso_cpu, gravacao_disco, fk_maquinaP, fk_empresaP) VALUES (%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE data_hora = %s, nome_processo = %s, uso_cpu = %s, gravacao_disco = %s, fk_maquinaP = %s, fk_empresaP = %s"
     valores = (processo['pid'], data_hora, processo['nome'], processo['uso_cpu'], processo['uso_disco'], 1, 1, data_hora, processo['nome'], processo['uso_cpu'], processo['uso_disco'], 1, 1)
 
@@ -86,38 +88,57 @@ def inserir_ou_atualizar_processo(processo):
         print(f"Falha ao inserir ou atualizar dados para o processo com PID {processo['pid']}: {err}")
         conexao.rollback()
 
+# Define a função para inserir dados de monitoramento em um banco SQL Server
 def inserir_dados_monitoramento(processo):
+    sql_server_cnx = pymssql.connect(
+        server='52.22.58.174',
+        database='nocline',
+        user='sa',
+        password='urubu100'
+    )
+
+    cursor_sql_server = sql_server_cnx.cursor()
+
+    data_hora = dt.now()
+
+    # Utilizando parâmetros na consulta preparada
+    sql_query = (
+        "MERGE INTO processos AS target "
+        "USING (VALUES (%s, %s, %s, %s, %s, %s, %s)) AS source "
+        "(pid, data_hora, nome_processo, uso_cpu, gravacao_disco, fk_maquinaP, fk_empresaP) "
+        "ON target.pid = (SELECT pid FROM processos WHERE pid = %s) "
+        "WHEN MATCHED THEN "
+        "UPDATE SET "
+        "data_hora = source.data_hora, "
+        "nome_processo = source.nome_processo, "
+        "uso_cpu = source.uso_cpu, "
+        "gravacao_disco = source.gravacao_disco, "
+        "fk_maquinaP = source.fk_maquinaP, "
+        "fk_empresaP = source.fk_empresaP "
+        "WHEN NOT MATCHED THEN "
+        "INSERT (pid, data_hora, nome_processo, uso_cpu, gravacao_disco, fk_maquinaP, fk_empresaP) "
+        "VALUES (%s, %s, %s, %s, %s, %s, %s);"
+    )
+
+    val = (
+        processo['pid'], data_hora, processo['nome'], processo['uso_cpu'],
+        processo['uso_disco'], 1, 1, processo['pid'],  # Repetições de valores para o MERGE
+        processo['pid'], data_hora, processo['nome'], processo['uso_cpu'],
+        processo['uso_disco'], 1, 1
+    )
+
     try:
-        mydb_server = pymssql.connect(server='52.22.58.174', database='nocline', user='sa', password='urubu100')
+        cursor_sql_server.execute(sql_query, val)
+        sql_server_cnx.commit()
+        print(cursor_sql_server.rowcount, "registros inseridos no banco")
+        print("\r\n")
+    except pymssql.Error as e:
+        print("Erro ao inserir dados no banco:", e)
+        sql_server_cnx.rollback()
+    print(cursor_sql_server.rowcount, "registros inseridos no banco")
+    print("\r\n")
 
-        try:
-            mycursor_server = mydb_server.cursor()
-            sql_query_server = "SELECT id_maquina, fk_empresaM FROM maquina WHERE hostname = %s;"
-            mycursor_server.execute(sql_query_server, (hostname,))
-            result = mycursor_server.fetchone()
-
-            if result:
-                try:
-                    if conexao.is_connected():
-                        sql_query = "INSERT INTO processos (pid, data_hora, nome_processo, uso_cpu, gravacao_disco, fk_maquinaP, fk_empresaP) VALUES (%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE data_hora = %s, nome_processo = %s, uso_cpu = %s, gravacao_disco = %s, fk_maquinaP = %s, fk_empresaP = %s"
-                        val = (processo['pid'], data_hora, processo['nome'], processo['uso_cpu'], processo['uso_disco'], 1, 1, data_hora, processo['nome'], processo['uso_cpu'], processo['uso_disco'], 1, 1)
-                        cursor.execute(sql_query, val)
-                        conexao.commit()
-                        print(cursor.rowcount, "registros inseridos no banco")
-                        print("\r\n")
-                    else:
-                        print("Não foi possível conectar ao banco.")
-                except mysql.connector.Error as e:
-                    print("Erro ao inserir dados no banco:", e)
-            else:
-                print("Resultado não encontrado.")
-        finally:
-            mycursor_server.close()
-            mydb_server.close()
-    except pymssql.OperationalError as e:
-        print("Erro ao conectar com o servidor MySQLServer:", e)
-
-# Exemplo de uso
+# Loop principal
 while True:
     uso_cpu_anterior = psutil.cpu_percent()
     processos_info = obter_info_processos()
@@ -128,8 +149,6 @@ while True:
         inserir_ou_atualizar_processo(processo)
         inserir_dados_monitoramento(processo)
 
-    # Exemplo de consulta por PID (substitua 1234 pelo PID desejado)
     consultar_processo_por_pid(4)
 
-    # Aguarda 10 segundos antes da próxima iteração
     time.sleep(10)
